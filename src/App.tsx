855c4c483348b89e7cf15f8563365c81bc575cc2
alexmuriuki <alexmuriukimaina254@gmail.com>
2023-01-22 20:03:46 +0300

    add navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.sass'
+import NavBar from './ui-common/NavBar'
 import ResumePreview from './ResumePreview'
 import { ContextProvider } from './context/ContextProvider'
 
 function App() {
 	return (
 		<div className='App'>
+			<NavBar />
 			<ContextProvider>
 				<ResumePreview />
 			</ContextProvider>
